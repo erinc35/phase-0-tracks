8ae225bf4fd5f6a2bd76175b18fa9f0499d70816
Erinc Emer <erincemer@Erincs-MBP.attlocal.net>
2016-06-27 08:43:56 -0700

    puppy

---
+++ ruby/puppy.rb
@@ -0,0 +1,16 @@
+#Husky
+
+SPECIES ------------------------
+Eskimotis Puppis
+
+CHARACTERISTICS ----------------
+Average Weight: 50
+Friendly: depends
+Eye color: white
+Color: white , black, gray
+
+BEHAVIOR -----------------------
+Jump
+Bite
+Protect
+Bark
